c6321b20f50e993de50e7c78cacab29621e1d536
RubberArchind <sean.makuto@gmail.com>
2024-05-18 22:44:14 +0700

    fix the cron bonus

--- cron/cron_bonus.php
+++ cron/cron_bonus.php
@@ -38,12 +38,14 @@ if ($result !== false && $result->num_rows > 0) {
             // $conn->query($sql3);
             $sql5 = sprintf("UPDATE cashback SET day_left = day_left-1 WHERE id = '%s'", $row['id']);
             $conn->query($sql5);
-            $sql3 = sprintf("INSERT INTO logs(user_id, type, amount, ref) VALUES ('%s', '%s', %f, '%s')", $row['user_id'], 'CASHBACK', $row['amount'] * 0.1, $row['id']);
+            $sql3 = sprintf("INSERT INTO logs(user_id, type, amount, ref) VALUES ('%s', '%s', %f, '%s')", $row['user_id'], 'CASHBACK', $row['amount'] * 0.01, $row['id']);
             $conn->query($sql3);
-            $sql4 = sprintf("INSERT INTO transaction(id, user_id, target_id, method, type, amount) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", "".rand(), $row['user_id'], $row['user_id'], 'Internal', 'CASHBACK', $row['amount'] * 0.1);
+            $sql4 = sprintf("INSERT INTO transaction(id, user_id, target_id, method, type, amount) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", "".rand(), $row['user_id'], $row['user_id'], 'Internal', 'CASHBACK', $row['amount'] * 0.01);
             $conn->query($sql4);
-            $sql2 = sprintf("UPDATE user SET balance_cashback = balance_cashback + %f WHERE user_id='%s'", $row['amount']*0.1, $row['user_id']);
+            $sql2 = sprintf("UPDATE user SET balance_cashback = balance_cashback + %f WHERE user_id='%s'", $row['amount']*0.01, $row['user_id']);
             $conn->query($sql2);
+            $sql6 = sprintf("UPDATE user SET balance_deposit = balance_deposit - %f WHERE user_id='%s'", $row['amount']*0.01, $row['user_id']);
+            $conn->query($sql6);
 
             $rows[] = array("day" => $dayleft, "user" => $row['user_id']);
         }
--- cron/overduepay.php
+++ cron/overduepay.php
@@ -29,14 +29,15 @@ if ($result !== false && $result->num_rows > 0) {
         $dayleft = $row['overdue'];
         while ($dayleft != 0) {
             //update balance_bonus user   
-            $m = new \Moment\Moment($row['start_date'], 'CET');            
+            $m = new \Moment\Moment($row['start_date'], 'CET');
             $c = $m->cloning()->addDays($dayleft);
 
-            $sql3 = sprintf("INSERT INTO logs(user_id, type, amount, time, ref) VALUES ('%s', '%s', %f, '%s', '%s')", $row['user_id'], 'CASHBACK', $row['amount'] * 0.1, $c->format("Y-m-d H:i:s"), $row['id']);
+            $divider = 0.01;
+            $sql3 = sprintf("INSERT INTO logs(user_id, type, amount, time, ref) VALUES ('%s', '%s', %f, '%s', '%s')", $row['user_id'], 'CASHBACK', $row['amount'] * $divider, $c->format("Y-m-d H:i:s"), $row['id']);
             $conn->query($sql3);
-            $sql4 = sprintf("INSERT INTO transaction(id, user_id, target_id, method, type, amount, time) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')", "".rand(), $row['user_id'], $row['user_id'], 'Internal', 'CASHBACK', $row['amount'] * 0.1, $c->format("Y-m-d H:i:s"));
+            $sql4 = sprintf("INSERT INTO transaction(id, user_id, target_id, method, type, amount, time) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s')", "" . rand(), $row['user_id'], $row['user_id'], 'Internal', 'CASHBACK', $row['amount'] * $divider, $c->format("Y-m-d H:i:s"));
             $conn->query($sql4);
-            $sql2 = sprintf("UPDATE user SET balance_cashback = balance_cashback + %f WHERE user_id='%s'", $row['amount']*0.1, $row['user_id']);
+            $sql2 = sprintf("UPDATE user SET balance_cashback = balance_cashback + %f WHERE user_id='%s'", $row['amount'] * $divider, $row['user_id']);
             $conn->query($sql2);
 
             $rows[] = array("day" => $dayleft, "date" => $c->format("Y-m-d H:i:s"));
